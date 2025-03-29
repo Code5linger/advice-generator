6f2d9ef575602e5e7eab596e2bddf1ecd708cd45
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2025-03-29 19:06:45 +0600

    !

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Adivce</h1>
+      <button>Get Advice</button>
+      <p>
+        You have read <strong>X</strong> advices!
+      </p>
+    </div>
+  );
+}
+
+export default App;
